fc223a80114d4444bfdf1e6ed6af430099f6cf86
igred8 <ivangadjev@gmail.com>
2022-11-12 08:04:41 -0800

    changed lib import to installed package

--- signal/setup.py
+++ signal/setup.py
@@ -1,8 +1,8 @@
 from setuptools import find_packages, setup
 
 setup(
-    name='src',
-    packages=find_packages(),
+    name='signal_process_lib',
+    packages=find_packages(where='src'),
     version='0.1.0',
     description='General outline on some signal processing techniques, mostly to learn and explore.',
     author='Ivan Gadjev',
